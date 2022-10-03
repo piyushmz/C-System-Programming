d402e93aaeea0839cb3261a93d1c6c414e6b8901
Piyush Thange <piyush@minuszero.in>
2022-10-03 18:27:52 +0530

    The pacakges to be installed for driveworks
    Signed-off-by: Piyush Thange <piyush@minuszero.in>

---
+++ build.sh
@@ -0,0 +1,5 @@
+apt install cmake make libgles2-mesa-dev pkg-config mesa-utils libglu1-mesa-dev freeglut3-dev mesa-common-dev libglew-dev libglfw3-dev libglm-dev libao-dev libmpg123-dev libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386 libxcursor-dev libegl1:i386 libxrandr-dev libxinerama-dev libxi-dev
+
+apt update
+apt upgrade
+
